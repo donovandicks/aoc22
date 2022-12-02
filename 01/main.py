551cfec17609cc1da2c98e49aedc9423eab3a42e
Donovan Dicks <donovan.dicks@datadoghq.com>
2022-12-01 20:42:00 -0500

    Change implementation

--- 01/main.py
+++ 01/main.py
@@ -1,13 +1,19 @@
+from collections import defaultdict
+
+
 def main():
     with open("input.txt", encoding="utf-8") as handler:
-        batches = handler.read().split("\n\n")
+        batch = 0
+        cals = defaultdict(int)
+        for line in handler.readlines():
+            if line == "\n":
+                batch += 1
+                continue
 
-    cals = []
-    for batch in batches:
-        cals.append(sum(int(cal) for cal in batch.split("\n") if cal))
+            cals[batch] += int(line.strip())
 
-    cals.sort(reverse=True)
-    print(cals[:3], sum(cals[:3]))
+    top_cals = sorted(list(cals.values()), reverse=True)
+    print(top_cals[:3], sum(top_cals[:3]))
 
 
 if __name__ == "__main__":
