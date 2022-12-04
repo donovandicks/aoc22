cd14d8632afdf0cc9ebbe16c7386ae98b82d8f28
Donovan Dicks <donovan.dicks@outlook.com>
2022-12-03 21:22:31 -0500

    Day 2 Question 2

--- 02/main.py
+++ 02/main.py
@@ -7,25 +7,33 @@ KEY = {
     "Z": 3,
 }
 
+WINNING_MOVES = {
+    1: 2,
+    2: 3,
+    3: 1,
+}
 
-def get_result(x: str, y: str) -> int:
-    opp, me = KEY[x], KEY[y]
-    if opp == me:
-        return 3
 
-    match (opp, me):
-        case (1, 2) | (2, 3) | (3, 1):
-            return 6
-        case _:
-            return 0
+def calc_score(x: str, y: str) -> int:
+    if y == "Y":
+        return 3 + KEY[x]
+
+    if y == "Z":
+        return 6 + WINNING_MOVES[KEY[x]]
+
+    if y == "X":
+        return 0 + WINNING_MOVES[KEY[x] % 3 + 1]
+
+    # unreachable
+    raise NotImplementedError()
 
 
 def main():
     score = 0
     with open("input.txt", encoding="utf-8") as h:
         for line in h.readlines():
-            opp, me = line.strip().split()
-            score += KEY[me] + get_result(opp, me)
+            opp, goal = line.strip().split()
+            score += calc_score(opp, goal)
 
     print(score)
 
