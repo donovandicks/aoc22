351031bd78a161ee4d1f16bdd5af0c302f9924b0
Donovan Dicks <donovan.dicks@outlook.com>
2022-12-04 16:27:28 -0500

    Day 4 Question 2

--- 04/main.py
+++ 04/main.py
@@ -31,11 +31,34 @@ def solve_1(data: str) -> int:
     return count
 
 
-def solve_2(data: str):
-    pass
+def solve_2(data: str) -> int:
+    count = 0
+    for line in data.split("\n"):
+        ((range_1_b, range_1_e), (range_2_b, range_2_e)) = parse_line(line)
+        if range_1_b in (range_2_b, range_2_e) or range_1_e in (range_2_b, range_2_e):
+            count += 1
+            continue
+
+        if range_2_b < range_1_b < range_2_e:
+            count += 1
+            continue
+
+        if range_2_b < range_1_e < range_2_e:
+            count += 1
+            continue
+
+        if range_1_b < range_2_b < range_1_e:
+            count += 1
+            continue
+
+        if range_1_b < range_2_e < range_1_e:
+            count += 1
+            continue
+
+    return count
 
 
 if __name__ == "__main__":
     input_data = load_input().strip()
     print(f"Solution 1: {solve_1(input_data)}")
-    solve_2(input_data)
+    print(f"Solution 2: {solve_2(input_data)}")
